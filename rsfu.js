a1a27762eb04ecf620035a5cc2c4aa7bffaec49e
Dmitrijs Minajevs <d3x133@gmail.com>
2017-05-26 16:37:12 +0300

    Updated for current voting
    They changed response code from 200 to 301 (why??)

--- rsfu.js
+++ rsfu.js
@@ -1,35 +1,36 @@
 var request = require('request');
 
-var workId = '73';
+var workId = '78';
 var votesCount = 100;
 
 
 var votesProcessed = 1;
 
 var options = {
-  url: 'https://www.rigassatiksme.lv/lv/konkurss/dzeltena-e-talona-jauna-dizaina-bernu-zimejumu-konkurss',
-  headers: {
-    'User-Agent': 'request',
-	'Content-Type' : 'application/x-www-form-urlencoded',
-	'Host' : 'www.rigassatiksme.lv',
-	'Origin' : 'https://www.rigassatiksme.lv',
-	'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
+  url: 'https://www.rigassatiksme.lv/ru/konkurs/konkurs-detskikh-risunkov-dlya-novogo-dizaina-zheltogo-e-talona-7',
+  headers:{
+    'User-Agent':'request',
+	'Content-Type' :'applicationx-www-form-urlencoded',
+	'Host' :'www.rigassatiksme.lv',
+	'Origin' :'httpswww.rigassatiksme.lv',
+	'User-Agent' :'Mozilla5.0 (Windows NT 10.0; WOW64) AppleWebKit537.36 (KHTML, like Gecko) Chrome51.0.2704.103 Safari537.36'
   },
-    form: {
-	action: 'vote',
-	workId: workId
+    form:{
+	action:'vote',
+	workId:workId
   }
 };
 
 function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log('success! ');
+	console.log('\033[2J');
+    if (!error) {
+        console.log('Success!');
     } else {
-		//console.log('error! ',error, response.statusCode);
-		console.log('\033[2J', votesProcessed++ + '/' + votesCount);
+		console.log('error! ',response, response.statusCode);
 	}
+	console.log(votesProcessed++ + ' / ' + votesCount);
 }
 
-for(var i = 0; i < votesCount; i++){
+for(var i = 0; i <votesCount; i++){
 	request.post(options, callback);
 }
